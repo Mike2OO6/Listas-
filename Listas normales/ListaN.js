79b27b153cf4ec87a30a646ccb4300a21fabc19a
Mike <miguelcamargolugo86@gmail.com>
2025-12-06 13:46:44 -0700

    Create ListaN.js

---
+++ Listas normales/ListaN.js
@@ -0,0 +1,8 @@
+const mostrarArreglo = (arreglo) => {
+    console.log(arreglo.join(' '));
+};
+
+const numeros = [5, 10, 15, 20, 25, 30, 35, 40];
+
+console.log("Elementos del arreglo:");
+mostrarArreglo(numeros);
